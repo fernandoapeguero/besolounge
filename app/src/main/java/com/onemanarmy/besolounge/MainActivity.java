112447f80bb495ee9cf151039f8ef53f024d18ef
fernando peguro paula <fernando peguro paula>
2017-02-15 11:37:34 -0500

    mi first commit

---
+++ app/src/main/java/com/onemanarmy/besolounge/MainActivity.java
@@ -0,0 +1,49 @@
+package com.onemanarmy.besolounge;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageButton callBeso = (ImageButton) findViewById(R.id.phone);
+
+        callBeso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent call = new Intent(Intent.ACTION_CALL);
+                call.setData(Uri.parse("tel:6468074522"));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(call);
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
